b9b1a87987a75ad5d069c611817ed3df4675837e
beehappy21 <chaiyanut.og@gmail.com>
2026-01-04 21:41:27 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,3 @@
-export { auth as middleware } from '@/lib/auth';
-
-// Don't invoke Middleware on some paths
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: [],
 };
